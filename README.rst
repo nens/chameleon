2d6f7b27bd2d6a37f7ea071c4403d7a50bdcefea
Berto Booijink <berto.booijink@nelen-schuurmans.nl>
2016-09-30 10:43:47 +0200

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,40 @@
+Chameleon
+=========
+
+This script monitors Lizard NXT build status and controls the traffic light.
+Of courese there's also some monkey dancing going on in here.
+
+WARNING! Pins 8, 10, 12, 16, 18 are physically wired to switches. NEVER set
+those pins to 'OUT' mode as it will short circuit the pin to ground, potentially
+burning the GPIO or the entire RasPi!
+
+Setup
+-----
+
+git clone git@github.com:nens/chameleon.git
+
+sudo python chameleon.py
+
+
+What does it mean?
+------------------
+
+Lights dictionary, Jenkins mode:
+
+PERLIN NOISE          | Building...
+ALL GREEN             | All builds successfull
+ALL ORANGE            | At least one build is unstable
+ALL RED               | At least one build failed
+MAIN GREEN, WALK RED  | All builds successfull, but do not walk in asking questions
+MAIN ORANGE, WALK RED | At least one build is unstable, do not walk in asking questions
+ORANGE FLASHING       | Get up, stand up
+ORANGE GOING BANANAS  | The light is trying to tell you something...
+ALL BLINKING TWICE    | Jenkins unreachable, could not determine build status
+ALL OFF               | Out of office, go home
+
+Lights dictionary, traffic mode:
+
+MAIN GREEN, WALK RED  | Drive, don't walk; push the button to cross
+MAIN RED, WALK GREEN  | Walk, don't drive; push the button when tired
+ALL OFF               | Out of office, go home
+
---
+++ chameleon.py
@@ -0,0 +1,278 @@
+# -*- coding: utf-8 -*-
+# (c) Nelen & Schuurmans, see LICENSE.rst.
+
+import RPi.GPIO as GPIO
+import random
+import time
+import urllib
+
+from datetime import datetime
+from threading import Thread
+
+
+STATUS = 'none'
+
+ALIVE = True
+
+NORTH_RED = 5
+NORTH_GREEN = 3
+EAST_RED = 19
+EAST_ORANGE = 21
+EAST_GREEN = 15
+SOUTH_RED = 11
+SOUTH_GREEN = 7
+WEST_RED = 13
+WEST_ORANGE = 29
+WEST_GREEN = 23
+
+ALL = [3, 5, 7, 11, 13, 15, 19, 21, 23, 29]
+
+REDS = [5, 19, 11, 13]
+ORANGES = [21, 29]
+GREENS = [3, 15, 7, 23]
+
+NORTH = [5, 3]
+EAST = [19, 21, 15]
+SOUTH = [11, 7]
+WEST = [13, 29, 23]
+
+NONE = []
+
+NEIGHBOURS = {
+  NORTH_RED : [NORTH_GREEN, EAST_RED, EAST_ORANGE],
+  NORTH_GREEN : [NORTH_RED, EAST_GREEN, EAST_ORANGE],
+  EAST_RED : [EAST_ORANGE, NORTH_RED],
+  EAST_ORANGE : [EAST_RED, EAST_GREEN] + NORTH,
+  EAST_GREEN : [EAST_ORANGE, NORTH_GREEN]
+}
+
+ACROSS = {
+  NORTH_RED : 11,
+  NORTH_GREEN : 7,
+  EAST_RED : 13,
+  EAST_ORANGE : 29,
+  EAST_GREEN : 23
+}
+
+ON = 8
+OFF = 10
+MANUAL = 12 
+AUTO = 16
+BUTTON = 18
+
+IN = [ON, OFF, MANUAL, AUTO, BUTTON]
+
+BUTTON_PRESSED = False
+
+MODE_OFF = 1
+MODE_MANUAL = 2
+MODE_STANDUP = 3
+MODE_LUNCH = 4
+MODE_STATUS = 5
+
+MORSE = {
+  'a':'.-',
+  'b':'-...',
+  'c':'-.-.',
+  'd':'-..',
+  'e':'.',
+  'f':'..-.',
+  'g':'--.',
+  'h':'....',
+  'i':'..',
+  'j':'.---',
+  'k':'-.-',
+  'l':'.-..',
+  'm':'--',
+  'n':'-.',
+  'o':'---',
+  'p':'.--.',
+  'q':'--.-',
+  'r':'.-.',
+  's':'...',
+  't':'-',
+  'u':'..-',
+  'v':'...-',
+  'w':'.--',
+  'x':'-..-',
+  'y':'-.--',
+  'z':'--..',
+}
+
+def fetchstatus():
+  global STATUS
+  while ALIVE:
+    #link = "http://buildbot.lizardsystem.nl/jenkins/view/Lizard%20NXT"
+    link = "https://jenkins.lizard.net/job/nens/job"
+    jobs = ["hydra-core", "lizard-client", "lizard-nxt"]
+    response = ""
+    for job in jobs:
+        f = urllib.urlopen("{}/{}/job/master/lastBuild/api/json?pretty=true".format(link, job))
+        response += f.read()
+    print(response)
+    quit()
+    new = 'none'
+    for str, status in [('Failed', 'broken'), ('Aborted', 'broken'), ('In progress', 'building'), ('Unstable', 'unstable'), ('Disabled', 'unstable'), ('Success', 'stable')]:
+      if ' tooltip="{}"'.format(str) in response:
+        new = status
+        break
+    STATUS = new
+    time.sleep(15)
+
+def setup():
+  GPIO.setmode(GPIO.BOARD)
+  for pin in ALL:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, False)
+  for pin in IN:
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  GPIO.add_event_detect(BUTTON, GPIO.RISING, callback=pressed, bouncetime=500)
+
+def setall(on):
+  if not get(ON):
+    on = NONE
+  for pin in ALL:
+    GPIO.output(pin, pin in on)
+
+def get(pin):
+  return True if GPIO.input(pin) == 0 else False
+
+def pressed(channel):
+  global BUTTON_PRESSED
+  BUTTON_PRESSED = True
+
+def getmode():
+  if not get(ON):
+    return MODE_OFF
+  if get(MANUAL):
+    return MODE_MANUAL
+  now = datetime.now()
+  if now.weekday() > 4:
+    return MODE_OFF
+  if now.hour < 7 or now.hour > 17:
+    return MODE_OFF
+  if now.hour == 12 and now.minute > 14 and now.minute < 30:
+    return MODE_STANDUP
+  if now.hour == 12 and now.minute > 29:
+    return MODE_LUNCH
+  return MODE_STATUS
+
+def loop(t, lists, mode):
+  while getmode() == mode:
+    walk(t, lists)
+
+def walk(t, lists):
+  for lights in lists:
+    setall(lights)
+    time.sleep(t)
+
+def dance(mode):
+  prev_last = EAST_GREEN
+  last = EAST_ORANGE
+  i = 0
+  while getmode() == mode and STATUS == 'building':
+    n = list(NEIGHBOURS[last])
+    n.remove(prev_last)
+    r = int(random.random() * len(n))
+    setall([last, ACROSS[last], n[r], ACROSS[n[r]]])
+    time.sleep(0.08)
+    setall([n[r], ACROSS[n[r]]])
+    time.sleep(0.08)
+    prev_last = last
+    last = n[r]
+    i += 1
+
+def traffic(mode):
+  global BUTTON_PRESSED
+  if getmode() != mode:
+    return
+  setall([NORTH_RED, EAST_GREEN, SOUTH_RED, WEST_GREEN])
+  while getmode() == mode and not BUTTON_PRESSED:
+    time.sleep(0.1)
+  BUTTON_PRESSED = False
+  time.sleep(1)
+  if getmode() != mode:
+    return
+  setall([NORTH_RED, EAST_ORANGE, SOUTH_RED, WEST_ORANGE])
+  time.sleep(2)
+  if getmode() != mode:
+    return
+  setall(REDS)
+  time.sleep(1)
+  if getmode() != mode:
+    return
+  setall([NORTH_GREEN, EAST_RED, SOUTH_GREEN, WEST_RED])
+  while getmode() == mode and not BUTTON_PRESSED:
+    time.sleep(0.1)
+  BUTTON_PRESSED = False
+  time.sleep(1)
+  if getmode() != mode:
+    return
+  for i in range(0, 5):
+    walk(0.2, [[EAST_RED, WEST_RED], [NORTH_GREEN, EAST_RED, SOUTH_GREEN, WEST_RED]])
+  if getmode() != mode:
+    return
+  setall(REDS)
+  time.sleep(1)
+
+def morse(unit, msg, lights, mode):
+  if getmode() == mode:
+    for char in msg:
+      if char == ' ':
+        walk(unit * 7, [NONE])
+        continue
+      for code in MORSE[char]:
+        if code == '.':
+          walk(unit, [lights])
+        if code == '-':
+          walk(unit * 3, [lights])
+        walk(unit, [NONE])
+      walk(unit * 2, [NONE])
+    walk(unit * 11, [NONE])
+
+def available():
+  now = datetime.now()
+  return now.hour < 17
+
+def dontwalk(lights):
+  return intersect(lights, NORTH + EAST + WEST) + [SOUTH_RED]
+
+def intersect(a, b):
+  return list(set(a) & set(b))
+
+def status(mode):
+  while getmode() == mode:
+    if STATUS == 'building':
+      dance(mode)
+      setall(NONE)
+    elif STATUS == 'stable':
+      setall(GREENS if available() else dontwalk(GREENS))
+    elif STATUS == 'unstable':
+      setall(ORANGES if available() else dontwalk(ORANGES))
+    elif STATUS == 'none':
+      HELP = [SOUTH_RED, WEST_ORANGE]
+      FLASH = HELP + NORTH + EAST
+      walk(0.02, [FLASH, HELP, HELP, HELP, FLASH, HELP])
+    else:
+      setall(REDS)
+    time.sleep(1)
+
+setup()
+t = Thread(target=fetchstatus)
+t.start()
+
+try:
+  while True:
+    loop(0.37, [NONE], MODE_OFF)
+    traffic(MODE_MANUAL)
+    loop(0.74, [NONE, ORANGES], MODE_STANDUP)
+    morse(0.125, 'carsten ga nou eens eten', ORANGES, MODE_LUNCH)
+    status(MODE_STATUS)
+except KeyboardInterrupt:
+  GPIO.cleanup()
+  ALIVE = False
+  print (" - Killed! Waiting for the kid to die...")
+except Exception as e:
+  GPIO.cleanup()
+  ALIVE = False
+  raise e
